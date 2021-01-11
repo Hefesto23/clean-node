e667cf38a16ee2979b889d6c83df11be9c28d9e2
Hefesto23 <vini.ra@outlook.com>
2021-01-11 11:03:33 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "./coverage",
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+
+};
