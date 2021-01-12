bfaa5e8df014c8ec47babd72320c4f4d2ae2dc33
Hefesto23 <vini.ra@outlook.com>
2021-01-12 16:03:28 -0300

    feat: add SignUpController

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup.controller'
+
+describe('SignUp Controller', () => {
+  it('should return 400 if no name is provided', () => {
+    const suControl = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = suControl.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
