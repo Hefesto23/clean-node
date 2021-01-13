0d123def30bb202f08fb1649a260cf21aaa44ae4
Hefesto23 <vini.ra@outlook.com>
2021-01-13 13:09:53 -0300

    refactor: create badRequest method to help handle errors

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,20 +1,15 @@
+import { badRequest } from './../errors/bad-request'
 import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from './../dtos/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (httpRequest.body.name === undefined) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (httpRequest.body.email === undefined) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
