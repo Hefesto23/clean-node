e3698e702b9878e4ba49efec3fdc583034612e4d
Hefesto23 <vini.ra@outlook.com>
2021-01-15 12:23:26 -0300

    feat: add use case AddAccount to SignUp Controller

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,3 +1,4 @@
+import { AddAccount } from './../../domain/usecases/add-account'
 import { serverError, badRequest } from './../errors/errors-helper'
 import { InvalidParamError, MissingParamError } from './../errors'
 import { EmailValidator } from './../validator/email-validator'
@@ -6,13 +7,15 @@ import { HttpRequest, HttpResponse } from './../dtos/http'
 
 export class SignUpController implements IController {
   private readonly emailValidator: EmailValidator
-  constructor (emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (httpRequest.body[field] === undefined) {
@@ -26,6 +29,11 @@ export class SignUpController implements IController {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       // this.logger(error)
       return serverError()
