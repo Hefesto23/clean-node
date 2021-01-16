4f0887b23f601c5d6da75be3d14626f19592f2f4
Hefesto23 <vini.ra@outlook.com>
2021-01-16 16:35:16 -0300

    test: mock validator library and check if method is being called with email param

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,32 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail: () => {
+    return true
+  }
+}))
 
 describe('Email Validator Adapter', () => {
-  it('Should return false if validator returns false', () => {
-    const eValAdpt = new EmailValidatorAdapter()
+  let eValAdpt: EmailValidatorAdapter
+  beforeEach(() => {
+    eValAdpt = new EmailValidatorAdapter()
+  })
+
+  it('should return true if validator returns true', () => {
+    const isValid = eValAdpt.isValid('valid_email@mail.com')
+    expect(isValid).toBeTruthy()
+  })
+
+  it('should return false if validator returns false', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = eValAdpt.isValid('invalid-email@mail.com')
     expect(isValid).toBeFalsy()
+  })
+
+  it('should call validator with correct email', () => {
+    const mock = jest.spyOn(validator, 'isEmail')
+    eValAdpt.isValid('anyEmail@mail.com')
+    expect(mock).toHaveBeenCalledWith('anyEmail@mail.com')
   })
 })
