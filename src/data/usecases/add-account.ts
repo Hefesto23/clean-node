b831147500c27bbd53848bbdf693d2f17db69c8a
Hefesto23 <vini.ra@outlook.com>
2021-01-16 21:11:19 -0300

    feat: create DbAddAccount and ensure add method use encrypter

---
+++ src/data/usecases/add-account.ts
@@ -0,0 +1,15 @@
+import { AccountModel } from '../../domain/models/account'
+import { Encrypter } from '../dtos/encrypter'
+import { AddAccount, AddAccountModel } from './../../domain/usecases/add-account'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise(resolve => resolve(null))
+  }
+}
